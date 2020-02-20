c56bbcad79de317c5823114a79a4bc6546d3ebc8
Prathik <prathik.kumar@gmail.com>
2020-02-19 20:39:55 -0600

    devBRanch commit

--- Abstract/src/abstractSample/Circle.java
+++ Abstract/src/abstractSample/Circle.java
@@ -21,8 +21,12 @@ public class Circle extends Shape
 		System.out.println("this is line 6");
 		return areaCircle;
 	}
+	
+	public void demo()
+	{
+		System.out.println("this is a demo function");
+	}
 
-	@Override
 	public double perimeter() {
 		// TODO Auto-generated method stub
 		double periCircle=2*pi*radius;
